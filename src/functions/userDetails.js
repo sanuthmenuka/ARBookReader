44d2a5083aa185f45fbd98b6e56e37f3113dc52d
Menuka L.D.S <104838986+sanuthmenuka@users.noreply.github.com>
2023-10-24 10:25:56 +0530

    minor update

--- src/functions/userDetails.js
+++ src/functions/userDetails.js
@@ -2,6 +2,7 @@ const UserDetails = async () => {
   const apiUrl = process.env.REACT_APP_API_URL;
 
   try {
+    console.log(`${apiUrl}`);
     const response = await fetch(`${apiUrl}/api/user/getUserDetails`);
     if (!response.ok) {
       throw new Error(`response was not ok. Status: ${response.status}`);
